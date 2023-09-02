9c2197c001626352d4f19620ea2a82ff5622a0d2
Neil Wang <neilywang3@gmail.com>
2023-09-02 17:08:48 -0400

    Initial Commit

---
+++ BetterAlarm/BetterAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterAlarmApp.swift
+//  BetterAlarm
+//
+//  Created by Neil Wang on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct BetterAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
